9e3ed8afd21923b4e3753e7bd018952ba93adc60
Paul Wilkinson <cremoni@gmail.com>
2024-12-12 19:33:55 +0100

    Fixed hydration error by removing SSR for gl view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 // pages/_app.tsx or pages/_app.js
+import dynamic from 'next/dynamic';
 import '@/index.css';
 import { useEffect } from 'react';
 import { useStore } from '@/store';
-import { EffectsView } from '@/components/EffectsView';
+// import { EffectsView } from '@/components/EffectsView';
 import { ControlPanel } from '@/components/ControlPanel';
 import { LfoPanel } from '@/components/LfoPanel';
+import { ControlSettings } from '@/store';
 
+const DynamicEffectsView = dynamic<{ controlSettingsParm: ControlSettings }>(() => import('@/components/EffectsView').then(mod => mod.EffectsView), {
+	ssr: false,
+  });
+  
 export default function Home() {
 	const { showControls, toggleShowControls, patch } = useStore();
 
@@ -31,7 +37,7 @@ export default function Home() {
 	return (
 		<div className="flex-col w-full h-full">
 			<div className="relative flex items-center justify-center w-full h-full">
-				<EffectsView controlSettingsParm={patch.controllerValues} />
+				<DynamicEffectsView controlSettingsParm={patch.controllerValues} />
 				{showControls && (
 					<>
 						<ControlPanel />
